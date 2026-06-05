1b9fc04b23817ae01f19436e7bbe956c0561a351
masashimorita <masashi.morita.mm@gmail.com>
2026-06-05 20:00:56 +0900

    :sparkles: Implement condition parser

--- lib/activerecord_callback_lens.rb
+++ lib/activerecord_callback_lens.rb
@@ -4,6 +4,7 @@ require "activerecord_callback_lens/version"
 require "activerecord_callback_lens/collector/callback_definition"
 require "activerecord_callback_lens/collector/callback_collector"
 require "activerecord_callback_lens/parser/condition_tree"
+require "activerecord_callback_lens/parser/condition_parser"
 require "activerecord_callback_lens/railtie" if defined?(Rails::Railtie)
 
 # Top-level namespace for the gem. Subsequent tasks append their requires above.
